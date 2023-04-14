8f78c9a12c360e0a2d94cb274d3a47da2ace013d
Éderson Marques <edersondesouzamarques@gmail.com>
2023-04-14 14:46:21 -0300

    utilização de arrow functions no script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,17 @@ import { getRandomInt, passGenerate } from './passGenerate.js';
 
 let passphrase = document.getElementById("pass"), generate_button = document.getElementById("generate_button"), copy_button = document.getElementById("copy_button"), pass_text = document.getElementById("pass_text");
 
-generate_button.addEventListener('click', showPass);
-copy_button.addEventListener('click', copyPass);
-
-
-function showPass() {
+const showPass = () => {
     pass_text.innerHTML = `A senha gerada é: `;
     passphrase.innerHTML = `${passGenerate()}`;
 }
 
-function copyPass() {
-    navigator.clipboard.writeText(passphrase.innerText);
-}
+const copyPass = () => navigator.clipboard.writeText(passphrase.innerText);
+
+
+generate_button.addEventListener('click', showPass);
+copy_button.addEventListener('click', copyPass);
+
+
 
 export {pass_text};
