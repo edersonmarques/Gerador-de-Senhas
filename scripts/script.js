17d2908e3c0c2b84f01dcf542802326583d3ec74
Éderson Marques <edersondesouzamarques@gmail.com>
2023-05-01 14:38:07 -0300

    adicionado elemento readonly pass

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,12 @@ import { getRandomInt, passGenerate } from './passGenerate.js';
 let passphrase = document.getElementById("pass"), generate_button = document.getElementById("generate_button"), copy_button = document.getElementById("copy_button"), pass_text = document.getElementById("pass_text");
 
 const showPass = () => {
-    pass_text.innerHTML = `A senha gerada é: `;
-    passphrase.innerHTML = `${passGenerate()}`;
+    passphrase.value = `${passGenerate()}`;
 }
 
-const copyPass = () => navigator.clipboard.writeText(passphrase.innerText);
-
+const copyPass = () => navigator.clipboard.writeText(passphrase.value);
 
 generate_button.addEventListener('click', showPass);
 copy_button.addEventListener('click', copyPass);
-
-
 
 export {pass_text};
